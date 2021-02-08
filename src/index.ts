033a96ab33f311fa99451c010618ec4e6db8eba6
Leonardo Marinho <50488688+dev-marinho@users.noreply.github.com>
2021-02-08 18:12:35 -0300

    Added Fetcher as default export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import Fetcher from '@/core/Fetcher';
 import * as Interfaces from '@/core/interfaces';
 import * as Types from '@/core/types';
 
-export { Fetcher, Interfaces, Types };
+export default Fetcher;
+
+export { Interfaces, Types };
