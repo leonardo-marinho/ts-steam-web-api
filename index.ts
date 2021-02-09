2ba9f05ba17cfe63c9f9ec7ab00f5d2544943612
Leonardo Marinho <50488688+dev-marinho@users.noreply.github.com>
2021-02-09 10:45:47 -0300

    Revert "Beta 1.0.0 (#4)"
    This reverts commit ba9612afea021893912471085eea9f596ab31ac9.

--- index.ts
+++
@@ -1,7 +0,0 @@
-import Fetcher from '@/Fetcher';
-import * as Interfaces from '@/interfaces';
-import * as Types from '@/types';
-
-export default Fetcher;
-
-export { Interfaces, Types };
--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-import tApiResponseCode from '@/types/tApiResponseCode';
-import tSteam64Id from '@/types/tSteam64Id';
-import tSteamWebApiMethods from '@/types/tSteamWebApiMethods';
-
-export { tApiResponseCode, tSteam64Id, tSteamWebApiMethods };
