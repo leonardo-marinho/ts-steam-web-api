437a39c56624be0b27a2f4b9bac46007c354350e
Leonardo Marinho <50488688+dev-marinho@users.noreply.github.com>
2021-02-17 05:41:09 -0300

    Revert "SteamID update (version 0.6.0) (#10)" (#11)
    This reverts commit ebf98f49e3a254c6364c7568fafebbee65f395a6.

--- index.ts
+++
@@ -1,8 +0,0 @@
-import Fetcher from '@/Fetcher';
-import SteamId from '@/SteamId';
-import * as Interfaces from '@/interfaces';
-import * as Types from '@/types';
-
-export default Fetcher;
-
-export { SteamId, Interfaces, Types };
